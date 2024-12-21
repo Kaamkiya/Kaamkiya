460fe437dc3595d63137320437497b4f7a687938
Kaamkiya <ziam3.1415@gmail.com>
2024-12-21 10:13:30 -0500

    init: Initial Commit

---
+++ README.md
@@ -0,0 +1,42 @@
+<pre>
+    _____________________
+   |                     |
+   | Hello! I'm Kaamkiya |
+   |_____________________|
+        ||
+(\___/) ||
+(• ^ •) ||
+/      =u
+
+about
+  Hello! I'm Kaamkiya, a backend developer who likes chocolate and terminals. I
+  mostly use Go, with some Zig and Python too.
+  Feel free to contribute to my projects, they're all open for help.
+
+skills
+  languages:   go, zig, py, js, ts, html, css
+  distros:     void, mint, debian
+  learning:    zig, c, java
+  other stuff: sqlite, linux, vim, neovim
+  javascript:  chartjs, p5js, astro, bun, deno, fresh
+
+social
+  mastodon:   <a href="https://hachyderm.io/@nonexistent">@nonexistent@hachyderm.io</a>
+  hackernews: <a href="https://news.ycombinator.com/user?id=kaamkiya">kaamkiya</a>
+
+misc
+  pronouns: he/him
+  wpm:      <!--S:MT_WPM--><!--E:MT_WPM--> with <!--S:MT_ACCURACY--><!--E:MT_ACCURACY-->
+
+stats
+  total commits:  <!--S:COMMIT_COUNT--><!--E:COMMIT_COUNT-->
+  stars earned:   <!--S:STARS_EARNED--><!--E:STARS_EARNED-->
+  contributed to: <!--S:CONTRIBUTED_TO--><!--E:CONTRIBUTED_TO-->
+  issues opened:  <!--S:ISSUES_OPENED--><!--E:ISSUES_OPENED-->
+  prs opened:     <!--S:PRS_OPENED--><!--E:PRS_OPENED-->
+  repo count:     <!--S:REPO_COUNT--><!--E:REPO_COUNT-->
+  gist count:     <!--S:GIST_COUNT--><!--E:GIST_COUNT-->
+  languages:
+    <!--S:LANGUAGES-->
+    <!--E:LANGUAGES-->
+</pre>
---
+++ scripts/main.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+import requests
+
+if __name__ == '__main__':
+    readme_file = os.getenv('readme')
+    if readme_file == '':
+        readme_file = 'README.md'
+    token = os.getenv('GH_TOKEN')
+    if token == '':
+        sys.exit(1)
+
+    headers = {
+        'Authorization': f'token {token}'
+    }
+
+    req = '''
+        query {
+            viewer {
+                createdAt
+                    issues {
+                        totalCount
+                    }
+                    pullRequests {
+                        totalCount
+                    }
+                    gists(first: 100) {
+                        totalCount
+                        nodes {
+                            stargazerCount
+                        }
+                    }
+                    repositories(affiliations: OWNER, first: 100) {
+                        totalCount
+                        totalDiskUsage
+                        nodes {
+                            languages(first: 40) {
+                                edges {
+                                    size
+                                    node {
+                                        name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    repositoriesContributedTo {
+                        totalCount
+                    }
+                    followers {
+                        totalCount
+                    }
+  	                following {
+                        totalCount
+                    }
+            }
+        }'''
+
+    res = requests.post('https://api.github.com/graphql', json.dumps({ 'query': req }), headers=headers)
+    if not res.ok:
+        print(res.status_code)
+        print(res.text)
+        sys.exit(1)
+
+    data = res.json()['data']['viewer']
+    print(data)
+
